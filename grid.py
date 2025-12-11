e6e97a06a0957b1863d91b1e6f0b706ac42a1d5a
Karfeinn <kayn.akabane@gmail.com>
2025-12-11 14:58:04 +0100

    detection img

--- grid.py
+++ grid.py
@@ -16,9 +16,18 @@ row3 = np.hstack(images[6:9])
 
 grid = np.vstack([row1, row2, row3])
 
+dict_img = {
+    ((0,0),(150,150)):"img 1",((151,0),(300,150)):"img 2",((301,0),(450,150)):"img 3",
+    ((0,151),(150,300)):"img 4",((151,151),(300,300)):"img 5",((301,151),(450,300)):"img 6",
+    ((0,301),(150,450)):"img 7",((151,301),(300,450)):"img 8",((301,301),(450,450)):"img 9"
+    }
+
 def draw_circle(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONUP:
         print(x,y)
+        for coord in dict_img:
+            if coord[0][0] < x < coord[1][0] and coord[0][1] < y < coord[1][1]:
+                print(dict_img[coord])
 
 cv2.namedWindow('CAPTCHA Grid')
 cv2.setMouseCallback('CAPTCHA Grid',draw_circle)
