d918bba9f8582db4b02e52bc99fbac2afc150066
Karfeinn <kayn.akabane@gmail.com>
2025-12-11 15:36:22 +0100

    ajout du cict activate

--- grid.py
+++ grid.py
@@ -2,7 +2,8 @@ import cv2
 import glob
 import numpy as np
 
-images = [cv2.imread(f"./Images/oiseau/resize/fichier_00{i}_resize.png") for i in range(9)]
+images_path = [(f"./Images/oiseau/resize/fichier_00{i}_resize.png") for i in range(9)]
+images = [cv2.imread(path) for path in images_path]
 
 # Vérification
 images = [img for img in images if img is not None]
@@ -17,20 +18,26 @@ row3 = np.hstack(images[6:9])
 grid = np.vstack([row1, row2, row3])
 
 dict_img = {
-    ((0,0),(150,150)):"img 1",((151,0),(300,150)):"img 2",((301,0),(450,150)):"img 3",
-    ((0,151),(150,300)):"img 4",((151,151),(300,300)):"img 5",((301,151),(450,300)):"img 6",
-    ((0,301),(150,450)):"img 7",((151,301),(300,450)):"img 8",((301,301),(450,450)):"img 9"
+    ((0,0),(150,150)):images_path[0],((151,0),(300,150)):images_path[1],((301,0),(450,150)):images_path[2],
+    ((0,151),(150,300)):images_path[3],((151,151),(300,300)):images_path[4],((301,151),(450,300)):images_path[5],
+    ((0,301),(150,450)):images_path[5],((151,301),(300,450)):images_path[7],((301,301),(450,450)):images_path[8]
     }
+
+dict_activate = {}
 
 def draw_circle(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONUP:
-        print(x,y)
         for coord in dict_img:
             if coord[0][0] < x < coord[1][0] and coord[0][1] < y < coord[1][1]:
-                print(dict_img[coord])
+                if dict_activate.get(dict_img[coord]) is None or dict_activate[dict_img[coord]] == 0:
+                    dict_activate[dict_img[coord]] = 1
+                else :
+                    dict_activate[dict_img[coord]] = 0
 
 cv2.namedWindow('CAPTCHA Grid')
 cv2.setMouseCallback('CAPTCHA Grid',draw_circle)
 cv2.imshow("CAPTCHA Grid", grid)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+print(dict_activate)
